3fc54af0eb5e19a50691a4a204154cd5eab418a5
Lance Dillon <riffraff169@yahoo.com>
2026-03-04 14:38:59 -0500

    types

--- scanner.c
+++ scanner.c
@@ -210,6 +210,8 @@ Token scanToken() {
             return makeToken(TOKEN_SLASH);
         case '*':
             return makeToken(TOKEN_STAR);
+        case '^':
+            return makeToken(TOKEN_CARET);
         case '!':
             return makeToken(
                     match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
