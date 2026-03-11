985624b45fc63466a755d9b9b471d6c10d77dd12
Lance Dillon <riffraff169@yahoo.com>
2026-03-11 13:55:07 -0400

    methods

--- debug.c
+++ debug.c
@@ -32,6 +32,16 @@ static int constantInstruction(const char* name, Chunk* chunk,
     return offset + 2;
 }
 
+static int invokeInstruction(const char* name, Chunk* chunk,
+        int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    uint8_t argCount = chunk->code[offset + 2];
+    printf("%-16s (%d args) %4d '", name, argCount, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 3;
+}
+
 static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1;
@@ -126,6 +136,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return jumpInstruction("OP_LOOP", -1, chunk, offset);
         case OP_CALL:
             return byteInstruction("OP_CALL", chunk, offset);
+        case OP_INVOKE:
+            return invokeInstruction("OP_INVOKE", chunk, offset);
         case OP_CLOSURE:
             {
                 offset++;
@@ -151,6 +163,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_RETURN", offset);
         case OP_CLASS:
             return constantInstruction("OP_CLASS", chunk, offset);
+        case OP_METHOD:
+            return constantInstruction("OP_METHOD", chunk, offset);
         case OP_DUP:
             return simpleInstruction("OP_DUP", offset);
         default:
