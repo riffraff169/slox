65be859188c6b2b307c27c6812a4e8c04b84bdf5
Lance Dillon <riffraff169@yahoo.com>
2026-03-11 12:36:15 -0400

    classes

--- chunk.h
+++ chunk.h
@@ -21,6 +21,8 @@ typedef enum {
     OP_GET_UPVALUE,
     OP_SET_UPVALUE,
     OP_EQUAL,
+    OP_GET_PROPERTY,
+    OP_SET_PROPERTY,
     OP_GREATER,
     OP_LESS,
     OP_ADD,
@@ -39,6 +41,7 @@ typedef enum {
     OP_CLOSURE,
     OP_CLOSE_UPVALUE,
     OP_RETURN,
+    OP_CLASS,
 } OpCode;
 
 typedef struct {
