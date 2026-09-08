ec6250593222bf9e7cf19e68edfe61b1af998450
Lance Dillon <riffraff169@yahoo.com>
2026-09-08 07:28:17 -0400

    bug on ifdef'ed out code

--- src/memory.c
+++ src/memory.c
@@ -45,7 +45,7 @@ void markObject(Obj* object) {
 
 #ifdef DEBUG_LOG_GC
     printf("%p mark ", (void*)object);
-    printValue(OBJ_VAL(object));
+    printValue(stdout, OBJ_VAL(object));
     printf("\n");
 #endif
 
@@ -75,7 +75,7 @@ static void markArray(ValueArray* array) {
 static void blackenObject(Obj* object) {
 #ifdef DEBUG_LOG_GC
     printf("%p blacken ", (void*)object);
-    printValue(OBJ_VAL(object));
+    printValue(stdout, OBJ_VAL(object));
     printf("\n");
 #endif
 
